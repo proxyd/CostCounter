bfb52be4f90d2bcac98e41fac53e9e080c4db235
proxyd <proxyd@gmail.com>
2015-05-20 00:58:16 +0300

    Update costcounter.js

--- js/costcounter.js
+++ js/costcounter.js
@@ -29,6 +29,9 @@ $('input, a[data-toggle="tab"]').on("keyup click change", function () {
         var today = new Date();
         var result = [];
         alert(new Date(Date.parse("01/05/2015")));
+        alert(new Date(Date.parse("01.05.2015")));
+        alert(new Date(Date.parse("01-05-2015")));
+        alert(new Date(Date.parse("2015-05-01")));
         arr.forEach(function (item) {
             var startDate = new Date(item.startDate);
             var endDate = new Date(item.endDate);
