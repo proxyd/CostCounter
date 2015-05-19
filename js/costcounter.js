e329a1cc070182013426e07e3440dd8d3cce9226
proxyd <proxyd@gmail.com>
2015-05-19 23:59:14 +0300

    Update costcounter.js

--- js/costcounter.js
+++ js/costcounter.js
@@ -34,7 +34,7 @@ $('input, a[data-toggle="tab"]').on("keyup click change", function () {
             var endDate = new Date(item.endDate);
             if (today >= startDate && today <= endDate) {
                 result = item.tariffs;
-                alert(item.tariffs[0].price);
+                alert("---");
             }
         });
         return result;
